c2a2a50d33af2dd13a5680bae4d76f90f365024b
nhanhtgcc200004 <nhanhtgcc200004@fpt.edu.vn>
2022-09-24 16:35:44 +0700

    fix connect

--- connect.php
+++ connect.php
@@ -5,8 +5,8 @@
 	//mysqli_close($Connect);*/
 	$Connect = pg_connect("postgres://ottdefdzvmlvfa:2a7349defe5b2e36a5d93691a84caa045a069879be0a7bb0a9b72441153b64e7@ec2-52-200-5-135.compute-1.amazonaws.com:5432/dbadql7hhbojeh
     ");
-    //$Connect = pg_connect("host=localhost port=5432 dbname=postgres");
-	//$Connect = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=123456");
+    //$Connect = pg_connect("host=ec2-52-200-5-135.compute-1.amazonaws.com port=5432 dbname=dbadql7hhbojeh");
+	//$Connect = pg_connect("host=ec2-52-200-5-135.compute-1.amazonaws.com port=5432 dbname=dbadql7hhbojeh user=ottdefdzvmlvfa password=2a7349defe5b2e36a5d93691a84caa045a069879be0a7bb0a9b72441153b64e7");
 	
     if (!$Connect) {
         die("Connection failed");
