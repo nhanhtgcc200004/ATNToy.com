804bc4ed75b3e7f70e67c0473a4a3b98c2209e87
nhanhtgcc200004 <nhanhtgcc200004@fpt.edu.vn>
2022-09-24 16:47:18 +0700

    update index

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
+<div>
 <?php
 include_once('connect.php');
 $res=pg_query($Connect,'select * from public.student');
-if(pg_num_rows($res)!=0)
-{
    while($row=pg_fetch_array($res,MYSQLI_ASSOC)){
-    
     ?>
     <p>this is <?php echo($row['name']) ?> a/an<?php echo($row['class'])?></p>
     <?php
 }
-}
 ?>
+</div>
