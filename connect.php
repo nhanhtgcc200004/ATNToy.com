c4444fe98ed9905e785782629ac7ba811c0f0325
nhanhtgcc200004 <nhanhtgcc200004@fpt.edu.vn>
2022-09-24 16:41:23 +0700

    s

--- connect.php
+++ connect.php
@@ -1,6 +1,5 @@
 <?php
-	$Connect = pg_connect("postgres://ottdefdzvmlvfa:2a7349defe5b2e36a5d93691a84caa045a069879be0a7bb0a9b72441153b64e7@ec2-52-200-5-135.compute-1.amazonaws.com:5432/dbadql7hhbojeh
-    ");
+	$Connect = pg_connect("postgres://ottdefdzvmlvfa:2a7349defe5b2e36a5d93691a84caa045a069879be0a7bb0a9b72441153b64e7@ec2-52-200-5-135.compute-1.amazonaws.com:5432/dbadql7hhbojeh");
     //$Connect = pg_connect("host=ec2-52-200-5-135.compute-1.amazonaws.com port=5432 dbname=dbadql7hhbojeh");
 	//$Connect = pg_connect("host=ec2-52-200-5-135.compute-1.amazonaws.com port=5432 dbname=dbadql7hhbojeh user=ottdefdzvmlvfa password=2a7349defe5b2e36a5d93691a84caa045a069879be0a7bb0a9b72441153b64e7");
 	
